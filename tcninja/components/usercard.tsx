13cb022abbde8c37dc653a44f28223b2d5affde1
Ori Daniel <ori.daniel2018@gmail.com>
2022-06-05 23:12:18 +0300

    feat: add basic style

--- tcninja/components/usercard.tsx
+++ tcninja/components/usercard.tsx
@@ -11,7 +11,7 @@ const UserCard = ({ userData }: { userData: IUserCardProps }) => {
     return (
         <Box sx={{
             display: 'flex', flexDirection: "row", justifyContent: 'flex-start', alignItems: 'center' ,
-            bgcolor: 'primary.main', p: 2, border: '1px solid grey',
+            bgcolor: 'primary.main', p: 2, border: '2px solid grey',
             boxShadow: 1, width: 500, fontWeight: 'fontWeightLight'
         }}>
             <Avatar alt={userData.username} src={userData.image_uri} />
