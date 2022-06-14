dd75ac7a489a47118ce140d83cab361a40992da2
Ori Daniel <ori.daniel2018@gmail.com>
2022-06-14 07:43:27 +0300

    feat: add user route

--- tcninja/pages/index.tsx
+++ tcninja/pages/index.tsx
@@ -5,17 +5,21 @@ import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import Button from '@mui/material/Button'
 import LoginButton from '../components/login-btn';
+import { useSession } from "next-auth/react"
 
 
 const Home: NextPage = () => {
+  const { data: session } = useSession()
   return (
     <>
       <h1>Trading Card Ninja</h1>
       <main>
         <LoginButton />
+        <Link href={  `user/${session?.user?.email}` }>
         <Button variant='contained'>
           User Data
         </Button>
+        </Link>
       </main>
 
     </>
