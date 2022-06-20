b12837d48a6f87c299ccaa701a336f978d303f94
Ori Daniel <ori.daniel2018@gmail.com>
2022-06-20 22:02:07 +0300

    feat: change layout

--- tcninja/components/layout/header/index.tsx
+++ tcninja/components/layout/header/index.tsx
@@ -3,10 +3,11 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
+import IconButton from '@mui/material/IconButton';
 import UserCard from './usercard';
 import Logo from "./logo";
 import HeaderSearch from './search'
-import { styled } from '@mui/material';
+import { Icon, styled } from '@mui/material';
 
 interface INavigationLinks {
   name: string,
@@ -32,14 +33,18 @@ const Header = ({height}: IStyledAppBarProps) => {
   return (
     <StyledAppBar position="fixed" height={height}>
       <Toolbar>
-        <Logo />
+      <IconButton href="./">
+      <Logo />
+      </IconButton>
         <Box sx={{ display: 'flex', flexDirection: 'row', flexGrow: 1, alignItems: 'center' }}>
           { navigationLinks.map((item, index) => (
+            <IconButton>
             <Link href={item.href} key={index}>
               <Typography variant="h6" component="div" sx={{ m: 2 }} >
                 {item.name}
               </Typography>
             </Link>
+            </IconButton>
           ))}
           <HeaderSearch />
       </Box>
